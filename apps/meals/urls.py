cc0f9faf0667fbf87085a635d5793adaa946d451
Michael Freimüller <michael@mfreimueller.com>
2025-03-19 13:46:29 +0100

    fix names of urlpatterns in meals/urls.py

--- apps/meals/urls.py
+++ apps/meals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MealListCreateView.as_view(), name='meals'),
-    path('<int:meal_id>', views.MealUpdateDeleteView.as_view(), name='meal-list-create')
+    path('', views.MealListCreateView.as_view(), name='meals-cr'),
+    path('<int:meal_id>', views.MealUpdateDeleteView.as_view(), name='meal-ud')
 ]
