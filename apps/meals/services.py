91d0af8d2cf8a0a1d1d3961d699692c98d9732ce
Michael Freimüller <michael@mfreimueller.com>
2025-03-19 10:14:33 +0100

    implement update of meals

--- apps/meals/services.py
+++ apps/meals/services.py
@@ -1,6 +1,7 @@
 from apps.suggest_logic.services import predict_meals
 from .models import Meal, MealSerialize
 from django.db import transaction
+from .utils import copy_meal_to
 import logging
 
 def get_all_meals():
@@ -31,3 +32,26 @@ def add_meals(meals):
             raise Exception(f"Meal with the name {meal} not found.")
     
     return created_meals
+
+def update_meal(meal_id, meal):
+    logger = logging.getLogger(__name__)
+
+    logger.debug(f"Updating meal with id {meal_id}...")
+
+    # we retrieve the meal identified by `id`, which will be
+    # updated with the contents found in the meal identified
+    # by the name of `meal`.
+
+    try:
+        meal_destination_object = Meal.objects.get(id=meal_id)
+    except Exception as e:
+        logger.error(f"No meal with id {meal_id} found!")
+        raise Exception(f"No meal with id {meal_id} found!")
+    
+    meal_source_object = Meal.objects.filter(meal=meal).first()
+    if not meal_source_object:
+        logger.error(f"No meal with name {meal} found!")
+        raise Exception(f"No meal with name {meal} found!")
+
+    meal_destination_object = copy_meal_to(meal_source_object, meal_destination_object)
+    meal_destination_object.save()
--- apps/meals/views.py
+++ apps/meals/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .services import add_meals, get_all_meals
+from .services import add_meals, get_all_meals, update_meal
 
 class MealListCreateView(APIView):
     """
@@ -23,15 +23,39 @@ class MealListCreateView(APIView):
 
         try:
             created_meals = add_meals(meals)
-        except:
+        except Exception as e:
             # at this point, either the database was locked or a meal wasn't found
-            return Response({ 'error': 'Invalid request.' }, status=400)
+            return Response({ 'error': e }, status=400)
 
         return Response({ 'meals': created_meals })
 
 class MealUpdateDeleteView(APIView):
-    def patch(self, request, meal_id):
-        pass
+    """
+    PUT /api/meals/{meal_id}
+
+    An API endpoint that updates the meal with the given id.
+    The idea is to allow the user to change meals in retrospect
+    in order to have the stored data represent the actual meal plan
+    for further suggestions.
+    """
+    def put(self, request, meal_id):
+        data = request.data
+
+        if 'meal' not in data:
+            return Response({ 'error': 'Excepted `meal` parameter.' }, status=400)
+
+        meal = data['meal']
+
+        try:
+            update_meal(meal_id, meal)
+        except Exception as e:
+            # either the ID was invalid or the meal wasn't found
+            if hasattr(e, 'message'):
+                return Response({ 'error': e }, status=400)
+            else:
+                return Response({ 'error': 'Invalid request.' }, status=400)
+
+        return Response({}, status=200)
 
     def delete(self, request, meal_id):
         pass
