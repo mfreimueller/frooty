6d7d7d5c7a520cce576dfc96a4a6ec3547775813
Michael Freimüller <michael@mfreimueller.com>
2025-03-19 14:27:39 +0100

    make name of meals unique to avoid duplication

--- apps/meals/models.py
+++ apps/meals/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from rest_framework import serializers
 
 class Meal(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     complexity = models.PositiveSmallIntegerField()
     soup  = models.PositiveSmallIntegerField(default=0)
     takeaway = models.PositiveSmallIntegerField(default=0)
