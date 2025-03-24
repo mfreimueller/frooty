726dd243176e2946e061c5da5de60bbfecbb49be
Michael Freimüller <michael@mfreimueller.com>
2025-03-24 11:11:19 +0100

    add history api to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/suggest/', include('apps.suggest.urls')),
+    path('api/meals/', include('apps.meals.urls')),
+    path('api/history/', include('apps.history.urls')),
     path('api/meals/', include('apps.meals.urls'))
 ]
