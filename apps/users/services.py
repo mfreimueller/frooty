9528ae2a0f486cf7d7581937f3a8e3b99c148902
Michael Freimüller <michael@mfreimueller.com>
2025-03-24 13:55:00 +0100

    remove family functionality from users app

--- apps/users/services.py
+++ apps/users/services.py
@@ -2,8 +2,7 @@ import logging
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db import transaction
-from .models import Family, FamilySerialize
-from .utils import str_to_snake_case
+from apps.families.services import FamilyService
 
 @transaction.atomic
 def register_user(username, email, password):
@@ -15,12 +14,7 @@ def register_user(username, email, password):
         logger.debug(f'Successfully created new user with name {username}')
 
         user_group_name = f'{user.username}_group'
-
-        family_group = Family.objects.create(name=user_group_name, family_name=user_group_name, personal=True)
-        family_group.save()
-
-        user.groups.add(family_group)
-        user.save()
+        FamilyService().create_group(user, user_group_name, True)
     except Exception as e:
         logger.exception(e)
         raise e
@@ -45,60 +39,3 @@ def update_user_password(user: User, new_password: str):
 
     user.set_password(new_password)
     user.save()
-
-def create_new_group(user: User, group_name: str):
-    internal_group_name = str_to_snake_case(group_name)
-
-    family_group = Family.objects.create(name=internal_group_name, family_name=group_name, personal=False)
-    family_group.save()
-
-    user.groups.add(family_group)
-    user.save()
-
-    return FamilySerialize(family_group).data
-
-def add_user_to_group(user: User, group_name: str, user_name: str):
-    group = user.groups.filter(name=group_name).first()
-    if group is None:
-        raise Exception(f"User is not part of a family named {group_name}.")
-    
-    family = Family.objects.filter(id=group.id).first()
-    if family is None:
-        raise Exception(f"There is no family with the name {group_name}.")
-    
-    if family.personal:
-        raise Exception(f"Not allowed to interact with personal group.")
-
-    user_to_add = User.objects.filter(username=user_name).first()
-    if user_to_add is None:
-        raise Exception(f"No user named {user_name} found.")
-    
-    user_to_add.groups.add(family)
-    user_to_add.save()
-
-def remove_user_from_group(user: User, group_name: str, user_name: str):
-    group = user.groups.filter(name=group_name).first()
-    if group is None:
-        raise Exception(f"User is not part of a family named {group_name}.")
-    
-    family = Family.objects.filter(id=group.id).first()
-    if family is None:
-        raise Exception(f"There is no family with the name {group_name}.")
-    
-    if family.personal:
-        raise Exception(f"Not allowed to interact with personal group.")
-    
-    user_to_remove = User.objects.filter(username=user_name).first()
-    if user_to_remove is None:
-        raise Exception(f"No user named {user_name} found.")
-    
-    if not user_to_remove.groups.filter(name=group_name).exists():
-        raise Exception(f"User named {user_name} is not a member of group {group_name}.")
-    
-    user_to_remove.groups.remove(family)
-    user_to_remove.save()
-
-    # delete the group if it becomes empty
-    if not family.user_set.exists():
-        family.delete()
-        group.delete() # necessary?
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .services import add_user_to_group, authenticate_user, create_new_group, logout_user, register_user, remove_user_from_group, update_user_password
+from .services import authenticate_user, logout_user, register_user, update_user_password
 from django.contrib.auth.decorators import login_not_required
 
 @login_not_required
@@ -55,50 +55,4 @@ def update_password(request):
         return Response({ 'error': 'Requires password.' }, status=400)
     
     update_user_password(user, password)
-    return Response({ 'success': True })
-
-@api_view(['POST'])
-def create_group(request):
-    data = request.data
-    user = request.user
-
-    group_name = data.get('name')
-    if group_name is None:
-        return Response({ 'error': 'Requires name of group.' }, status=400)
-    
-    group = create_new_group(user, group_name)
-    return Response({ 'group': group })
-
-@api_view(['POST'])
-def add_to_group(request):
-    data = request.data
-    user = request.user
-
-    group_name = data.get('name')
-    user_name = data.get('user')
-    if not all([ group_name, user_name ]):
-        return Response({ 'error': 'Requires names of user and group.' }, status=400)
-    
-    try:
-        add_user_to_group(user, group_name, user_name)
-    except Exception as e:
-        return Response({ 'error': e }, status=400)
-    
-    return Response({ 'success': True })
-
-@api_view(['DELETE'])
-def remove_from_group(request):
-    data = request.data
-    user = request.user
-
-    group_name = data.get('name')
-    user_name = data.get('user')
-    if not all([ group_name, user_name ]):
-        return Response({ 'error': 'Requires names of user and group.' }, status=400)
-    
-    try:
-        remove_user_from_group(user, group_name, user_name)
-    except Exception as e:
-        return Response({ 'error': e }, status=400)
-    
     return Response({ 'success': True })
