4345799a7b41359a9bb61d25243d63d343807c33
Michael Freimüller <michael@mfreimueller.com>
2025-03-24 14:47:22 +0100

    add relation between history and family to allow for many histories for many families

--- apps/history/models.py
+++ apps/history/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from rest_framework import serializers
+from apps.families.models import Family
 
 class History(models.Model):
     meal = models.CharField(max_length=200)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE, related_name='histories')
 
 class HistorySerialize(serializers.Serializer):
     id = serializers.IntegerField()
