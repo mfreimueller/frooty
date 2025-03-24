2d8f4bd986ee5eebbe0806c4ffdd2ed5ae3eaae7
Michael Freimüller <michael@mfreimueller.com>
2025-03-24 15:45:15 +0100

    fix history tests

--- apps/history/tests.py
+++ apps/history/tests.py
@@ -1,16 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import History
-from .services import add_history, delete_history_item, get_all_history, update_history_item
+from .services import HistoryService
+from apps.families.models import Family
 
 # TODO: Add test cases for duplicated names.
 
 class HistoryServiceTestCase(TestCase):
     def setUp(self):
+        self.users = [
+            User.objects.create_user(username='user', email='email@email.com', password='123'),
+            User.objects.create_user(username='user2', email='email@email.com', password='123')
+        ]
+
+        self.family = Family.objects.create(family_name="My Family", personal=False, owner=self.users[0])
+        self.users[0].groups.add(self.family)
+        self.users[0].save()
+
         self.history = [
-            History.objects.create(meal="Brotjause"),
-            History.objects.create(meal="Sushi"),
-            History.objects.create(meal="Leberkässemmel"),
-            History.objects.create(meal="Fisch")
+            History.objects.create(meal="Brotjause", family=self.family),
+            History.objects.create(meal="Sushi", family=self.family),
+            History.objects.create(meal="Leberkässemmel", family=self.family),
+            History.objects.create(meal="Fisch", family=self.family)
         ]
 
     """
@@ -27,14 +38,17 @@ class HistoryServiceTestCase(TestCase):
         return super().tearDown()
 
     def test_add_history_items(self):
-        meals = add_history([ "Toast", "Eierspeise" ])
+        meals = HistoryService().add_history(self.users[0], [ "Toast", "Eierspeise" ], self.family.id)
         self.assertEqual(len(meals), 2)
         self.assertEqual(meals[0]['meal'], "Toast")
         self.assertEqual(meals[1]['meal'], "Eierspeise")
 
     def test_get_all_history(self):
-        history_items = get_all_history()
+        history_items = HistoryService().get_all_of_family(self.users[0], self.family.id)
         self.assertEqual(len(history_items), len(self.history))
+
+    def test_get_all_for_unauthorized_user(self):
+        self.assertRaises(Exception, HistoryService().get_all_of_family, self.users[1], self.family.id)
 
     """
     This test makes sure that an existing history item is
@@ -43,15 +57,19 @@ class HistoryServiceTestCase(TestCase):
     def test_delete_existing_history_item(self):
         # we don't test add_history here, we assume that it's working,
         # as we already covered this above
-        meal = add_history([ "Toast" ])
+        meal = HistoryService().add_history(self.users[0], [ "Toast" ], self.family.id)
         history_id = meal[0]['id']
 
-        try:
-            delete_history_item(history_id)
-        except Exception as e:
-            self.fail(f"delete_history_item() raised an exception: {e}")
-
+        HistoryService().delete_history_item(self.users[0], history_id)
         self.assertIsNone(History.objects.filter(id=history_id).first())
+
+    def test_delete_existing_history_item_for_unauthorized_user(self):
+        # we don't test add_history here, we assume that it's working,
+        # as we already covered this above
+        meal = HistoryService().add_history(self.users[0], [ "Toast" ], self.family.id)
+        history_id = meal[0]['id']
+        
+        self.assertRaises(Exception, HistoryService().delete_history_item, self.users[1], history_id)
 
     """
     This test makes sure that attempting to delete a
@@ -59,7 +77,7 @@ class HistoryServiceTestCase(TestCase):
     """
     def test_delete_missing_history_item(self):
         history_id = -999
-        self.assertRaises(Exception, delete_history_item, history_id)
+        self.assertRaises(Exception, HistoryService().delete_history_item, self.users[0], history_id)
 
     """
     This test makes sure that updating existing history item works 
@@ -68,16 +86,21 @@ class HistoryServiceTestCase(TestCase):
     def test_update_existing_history_item(self):
         # we don't test add_meals here, we assume that it's working,
         # as we already covered this above
-        item = add_history([ "Toast" ])
+        item = HistoryService().add_history(self.users[0], [ "Toast" ], self.family.id)
 
         history_id = item[0]['id']
         
-        try:
-            update_history_item(history_id, 'Veganer Burger')
-        except Exception as e:
-            self.fail(f"update_meal() raised an exception: {e}")
-
+        HistoryService().update_history_item(self.users[0], history_id, 'Veganer Burger')
         self.assertEqual(History.objects.filter(id=history_id).first().meal, 'Veganer Burger')
+
+    def test_update_existing_history_item_for_unauthorized_user(self):
+        # we don't test add_meals here, we assume that it's working,
+        # as we already covered this above
+        item = HistoryService().add_history(self.users[0], [ "Toast" ], self.family.id)
+
+        history_id = item[0]['id']
+
+        self.assertRaises(Exception, HistoryService().update_history_item, self.users[1], history_id, 'Veganer Burger')
 
     """
     This test makes sure that attempting to update a non existing history item
@@ -85,4 +108,4 @@ class HistoryServiceTestCase(TestCase):
     """
     def test_update_missing_history_item(self):
         history_id = -999
-        self.assertRaises(Exception, update_history_item, history_id, 'Veganer Burger')
+        self.assertRaises(Exception, HistoryService().update_history_item, self.users[0], history_id, 'Veganer Burger')
