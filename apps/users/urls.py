02f15a57025a12fa02f2816dfd284b6557cf2230
Michael Freimüller <michael@mfreimueller.com>
2025-03-25 11:48:52 +0100

    add missing url patterns for users app

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('login', views.authenticate, name='login'), # TODO: move to auth
+    path('logout', views.logout, name='logout'),
+    path('password', views.update_password, name='update_password')
 ]
